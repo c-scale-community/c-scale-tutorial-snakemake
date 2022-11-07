c091f612304d26e7a7accded3c70de1a49b0f9e0
Benjamin Schumacher <benjamin.schumacher@eodc.eu>
2022-11-07 15:00:30 +0000

    new example plot CORINE LAND COVER

---
+++ example_clc_plot.py
@@ -0,0 +1,15 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import numpy as np
+output_data4 = xr.open_dataset("./demo_data/U2000_CLC1990_V2020_20u1.tif")
+min_lon = 1909850
+max_lon = 1700150
+
+min_lat = 2700150 
+max_lat = 2900000 
+
+
+cropped_ds = output_data4.sel(band= 1, x=slice(min_lat,max_lat), y=slice(min_lon,max_lon))
+
+arr = np.array(cropped_ds.to_array())
+plt.imshow(arr[0])
