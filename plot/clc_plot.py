d2ab14c516825ced23a3844f2a0461ae6966ddce
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2022-11-08 09:28:19 +0100

    update path and add savefig() to clc_plot.py

--- plot/clc_plot.py
+++ plot/clc_plot.py
@@ -1,7 +1,7 @@
 import xarray as xr
 import matplotlib.pyplot as plt
 import numpy as np
-output_data4 = xr.open_dataset("./demo_data/U2000_CLC1990_V2020_20u1.tif")
+output_data4 = xr.open_dataset("../data/u2000_clc1990_v2020_20u1_raster100m/DATA/U2000_CLC1990_V2020_20u1.tif")
 min_lon = 1909850
 max_lon = 1700150
 
@@ -13,3 +13,4 @@ cropped_ds = output_data4.sel(band= 1, x=slice(min_lat,max_lat), y=slice(min_lon
 
 arr = np.array(cropped_ds.to_array())
 plt.imshow(arr[0])
+plt.savefig('output.png')
